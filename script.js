d8985fac176c54037b2d5fb82aa7676d021a585f
oybesila <oybekshukurullayv111@gmail.com>
2023-09-28 15:08:34 +0500

    brinchi proyect

---
+++ script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+  });
+  
